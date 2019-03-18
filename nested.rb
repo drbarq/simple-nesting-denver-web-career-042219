d99fb7c7057331002a1c305870b340215ecaa830
Joe Tustin <j.tustin@gmail.com>
2019-03-18 21:22:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,10 +76,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-				:yukihiro_matsumoto => {
-			   	:known_for => "Ruby",
-			    :languages => ["LISP", "C"]
-			 }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -94,7 +90,12 @@ def adding_matz
         }
      }
 
-
+		 programmer_hash[
+		 :yukihiro_matsumoto => {
+	 		:known_for => "Ruby",
+	 		:languages => ["LISP", "C"]
+	  }
+]
 end
 
 def changing_alan
