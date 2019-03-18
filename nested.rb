c22d418eaf01775b230bb2806a19f8b323943a5d
Joe Tustin <j.tustin@gmail.com>
2019-03-18 21:29:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -141,6 +141,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] >> "assembly"
+		 programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
+
+		 puts programmer_hash[:dennis_ritchie]
+		 return programmer_hash
 
 end
